45a13d945c15f02f29831a66875dc198fd4555d0
4ndysnipez <snipez.ebox@googlemail.com>
2013-04-24 15:55:22 +0200

    ungenutzte sachen auskommentiert

--- ZgenMobil/Controller/PickerViewController.cs
+++ ZgenMobil/Controller/PickerViewController.cs
@@ -15,24 +15,27 @@ namespace ZgenMobil
 		public PickerViewController (params string[] selektionen)
 		{
 			this._selektionen = selektionen;
-	//		_doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, null, null);
-			//_doneButton.Clicked += (object sender, EventArgs e) => {
-			//	_actionSheet.DismissWithClickedButtonIndex (0, true); 
 
-		//	var flexibleSpace = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace);
+		/*
+			_doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, null, null);
+			_doneButton.Clicked += (object sender, EventArgs e) => {
+			_actionSheet.DismissWithClickedButtonIndex (0, true); 
 
-		//	_toolbar = new UIToolbar();//new RectangleF(0, 0, _actionSheet.Frame.Width, 10));
-		//	_toolbar.BarStyle = UIBarStyle.Black;
-		//	_toolbar.Translucent = true;
-			//_toolbar.SetItems(new [] { _doneButton } , false);
+			var flexibleSpace = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace);
 
-			//_toolbar.Items = new UIBarButtonItem[] {
-			//	new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace, null, null),
-			//	_doneButton
-			//};
+			_toolbar = new UIToolbar();//new RectangleF(0, 0, _actionSheet.Frame.Width, 10));
+			_toolbar.BarStyle = UIBarStyle.Black;
+			_toolbar.Translucent = true;
+			_toolbar.SetItems(new [] { _doneButton } , false);
+
+			_toolbar.Items = new UIBarButtonItem[] {
+			new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace, null, null),
+			_doneButton
+			};
 
 
-			//_toolbar.SizeToFit();
+			_toolbar.SizeToFit();
+		*/
 
 		}
 
